12f654bb3374cdea8213997e982f83665b284486
sftan02 <104035248+sftan02@users.noreply.github.com>
2022-09-26 16:04:12 +0800

    Add files via upload

---
+++ Assignment 2/js/vegalite.js
@@ -0,0 +1,4 @@
+var vg_1 = "js/visualization.vl.json";
+vegaEmbed("#vis", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
