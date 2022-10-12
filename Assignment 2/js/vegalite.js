aec0f32e0dfc8d8d776c2b24e5966336c8840490
sftan02 <104035248+sftan02@users.noreply.github.com>
2022-10-12 10:51:06 +0800

    Delete Assignment 2 directory

--- Assignment 2/js/vegalite.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "js/visualization.vl.json";
-vegaEmbed("#vis", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
