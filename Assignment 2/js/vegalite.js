f6c3ac14c756a8841d2dd14f5286fa3b06f10b68
sftan02 <104035248+sftan02@users.noreply.github.com>
2022-10-18 19:21:07 +0800

    Add files via upload

---
+++ Assignment 2/js/vegalite.js
@@ -0,0 +1,15 @@
+var vis_1 = "js/map.json";
+var vis_2 = "js/top_countries.json";
+var vis_3 = "js/grouped.json";
+
+vegaEmbed("#vis1", vis_1, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#vis2", vis_2, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#vis3", vis_3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
